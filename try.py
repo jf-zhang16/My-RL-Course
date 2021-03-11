b824ebb9b70d6e9e3038454ff1dbc69e30249244
Eben <986519126@qq.com>
2021-03-11 22:51:01 +0800

    Update try.py

--- try.py
+++ try.py
@@ -1,6 +1,78 @@
 import numpy as np
 
-for i in range(10):
-    a = np.random.random()
-    print(a)
+np.random.seed(1)
 
+# for i in range(10):
+#     a = np.random.random()
+#     print(a)
+
+# b = ['up', 'down', 'left', 'right']
+# actions = {'up': np.array([0, -1]), 'down': np.array([0, 1]), 'left': np.array([-1, 0]), 'right': np.array([1, 0])}
+# for j in range(5):
+#     c = np.random.choice(b)
+#     print(c)
+#     print(actions[c])
+#
+# def step(a, b):
+#     c = a + b
+#     d = a * b
+#     return c, d
+#
+# x, y = step(2,4)
+# print(x)
+# print(y)
+
+
+# a = list(range(5))
+# print(a)
+# print(a.index(3))
+
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
+import random
+
+# parameters
+gamma = 0.6 # discounting rate
+rewardSize = -1
+gridSize = 4
+terminationStates = [[0,0], [gridSize-1, gridSize-1]]
+actions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+numIterations = 10000
+
+V = np.zeros((gridSize, gridSize))
+returns = {(i, j):list() for i in range(gridSize) for j in range(gridSize)}
+print(returns)
+deltas = {(i, j):list() for i in range(gridSize) for j in range(gridSize)}
+states = [[i, j] for i in range(gridSize) for j in range(gridSize)]
+print(states[:-1])
+
+
+# utils
+def generateEpisode():
+    initState = random.choice(states[1:-1])
+    episode = []
+    while True:
+        if list(initState) in terminationStates:
+            return episode
+        action = random.choice(actions)
+        finalState = np.array(initState)+np.array(action)
+        if -1 in list(finalState) or gridSize in list(finalState):
+            finalState = initState
+        episode.append([list(initState), action, rewardSize, list(finalState)])
+        initState = finalState
+
+for it in tqdm(range(numIterations)):
+    episode = generateEpisode()
+    G = 0
+    #print(episode)
+    for i, step in enumerate(episode[::-1]):
+        G = gamma*G + step[2]
+        if step[0] not in [x[0] for x in episode[::-1][len(episode)-i:]]:
+            idx = (step[0][0], step[0][1])
+            returns[idx].append(G)
+            newValue = np.average(returns[idx])
+            deltas[idx[0], idx[1]].append(np.abs(V[idx[0], idx[1]]-newValue))
+            V[idx[0], idx[1]] = newValue

8911b8cf0a5dbf21bd6625c48888d42892cbaadd
Eben Hu <EbenHu@outlook.com>
2021-03-12 10:37:04 +0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -2,6 +2,13 @@ import numpy as np
 
 a = [[[2, 1], [1, 0], -1, [3, 1]], [[3, 1], [0, -1], -1, [3, 0]], [[3, 0], [1, 0], -1, [3, 0]]]
 print(len(a))
-print(a)
-print(a[0])
-print(a[0][2])
+# print(a)
+# print(a[0])
+# print(a[0][2])
+print(a[::-1])
+print(a[::-1][3:])
+print(a[::-1][2:])
+print(a[::-1][1:])
+
+
+

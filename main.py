0556f1838633d2c533f521b91fbba4c29d7c43a3
Eben Hu <EbenHu@outlook.com>
2021-03-12 14:48:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ for a in range(iterations):
     episode = play.generate_episode()
     G = 0
     for i, step in enumerate(episode[::-1]):
-        # episode = []  # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
+        # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
         G = gamma*G + step[2]
-        if step[0] not in [x[0] for x in episode[::-1][i+1:]]:
-            idx = (step[0][0], step[0][1])
+        if step[0] not in [x[0] for x in episode[::-1][i+1:]]:  # justify whether step[0] is the first meet
+            idx = (step[0][0], step[0][1])  # idx is (x, y)
             returns[idx].append(G)
             newValue = np.average(returns[idx])
-            deltas[idx[0], idx[1]].append(np.abs(values[idx[0], idx[1]]-newValue))
+            # deltas[idx[0], idx[1]].append(np.abs(values[idx[0], idx[1]]-newValue))
             values[idx[0], idx[1]] = newValue
-        # else:
+
+print(values)
 

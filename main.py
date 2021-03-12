d092887acc4339605ffab0145eb81cc697bf2f6a
Eben Hu <EbenHu@outlook.com>
2021-03-12 10:37:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from grid import Grid
 import numpy as np
 
-# state-values predicted by Monte Carlo
+# state-values predicted by first view Monte Carlo
 play = Grid()
 iterations = 10
 values = np.zeros((4, 4))
@@ -13,10 +13,13 @@ for a in range(iterations):
     episode = play.generate_episode()
     G = 0
     for i, step in enumerate(episode[::-1]):
+        # episode = []  # episode contains [s0, action0, reward1,s1, s1, action1],...[sT-1, actionT-1, final_reward, sT]
         G = gamma*G + step[2]
-        if step[0] not in [x[0] for x in episode[::-1][len(episode)-i:]]:
+        if step[0] not in [x[0] for x in episode[::-1][i+1:]]:
             idx = (step[0][0], step[0][1])
             returns[idx].append(G)
             newValue = np.average(returns[idx])
             deltas[idx[0], idx[1]].append(np.abs(values[idx[0], idx[1]]-newValue))
             values[idx[0], idx[1]] = newValue
+        # else:
+
